e0b4cc0e1c405493719db456b0d0f35568972e75
Indraneel Rajeevan <e.indraneel@gmail.com>
2024-03-27 18:19:20 +0530

    copyright notice

--- app/lib/src/constants.dart
+++ app/lib/src/constants.dart
@@ -1,3 +1,8 @@
+// Copyright (c) 2024, Indraneel Rajeevan
+// Copyright (c) 2024, Deepen Shrestha
+// 
+// SPDX-License-Identifier: BSD-3-Clause
+
 import 'package:flutter/material.dart' show Icons;
 
 const appName = 'Archaic Keys';
