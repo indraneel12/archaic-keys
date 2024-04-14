ff945c6ed9285768f7683930dec1e4bf38da054b
Indraneel Rajeevan <e.indraneel@gmail.com>
2024-04-15 02:49:15 +0530

    implemented Basic Consonant keys (Malayalam)

--- app/lib/src/widgets/keyboards/custom_keyboard.dart
+++ app/lib/src/widgets/keyboards/custom_keyboard.dart
@@ -20,8 +20,8 @@ import 'malayalam/malayalam_keyboard.dart';
 class CustomKeyboard extends StatelessWidget {
   const CustomKeyboard({super.key});
 
-  static const minWidth = AppDimensions.maxWidth * 0.90;
-  static const minHeight = AppDimensions.minHeight * 0.90;
+  static const minWidth = AppDimensions.maxWidth * 0.70;
+  static const minHeight = AppDimensions.minHeight * 0.70;
   static const maxWidth = AppDimensions.maxWidth * 1.00;
   static const maxHeight = AppDimensions.maxHeight * 1.00;
 
--- app/lib/src/widgets/keyboards/malayalam/malayalam_keyboard.dart
+++ app/lib/src/widgets/keyboards/malayalam/malayalam_keyboard.dart
@@ -207,6 +207,131 @@ class MalayalamKeyboard extends StatelessWidget {
         ).withGridPlacement(rowStart: 1, columnStart: 17),
 
         // ------------------------Basic-Consonants------------------------
+        const MalayalamKey(
+          value: 'ക',
+          devanagariLabel: 'क',
+          iso15919Label: 'k',
+        ).withGridPlacement(rowStart: 3, columnStart: 0),
+        const MalayalamKey(
+          value: 'ഖ',
+          devanagariLabel: 'ख',
+          iso15919Label: 'kh',
+        ).withGridPlacement(rowStart: 3, columnStart: 1),
+        const MalayalamKey(
+          value: 'ഗ',
+          devanagariLabel: 'ग',
+          iso15919Label: 'g',
+        ).withGridPlacement(rowStart: 3, columnStart: 2),
+        const MalayalamKey(
+          value: 'ഘ',
+          devanagariLabel: 'घ',
+          iso15919Label: 'gh',
+        ).withGridPlacement(rowStart: 3, columnStart: 3),
+        const MalayalamKey(
+          value: 'ങ',
+          devanagariLabel: 'ङ',
+          iso15919Label: 'ṅ',
+        ).withGridPlacement(rowStart: 3, columnStart: 4),
+        const MalayalamKey(
+          value: 'ച',
+          devanagariLabel: 'च',
+          iso15919Label: 'c',
+        ).withGridPlacement(rowStart: 4, columnStart: 0),
+        const MalayalamKey(
+          value: 'ഛ',
+          devanagariLabel: 'छ',
+          iso15919Label: 'ch',
+        ).withGridPlacement(rowStart: 4, columnStart: 1),
+        const MalayalamKey(
+          value: 'ജ',
+          devanagariLabel: 'ज',
+          iso15919Label: 'j',
+        ).withGridPlacement(rowStart: 4, columnStart: 2),
+        const MalayalamKey(
+          value: 'ഝ',
+          devanagariLabel: 'झ',
+          iso15919Label: 'jh',
+        ).withGridPlacement(rowStart: 4, columnStart: 3),
+        const MalayalamKey(
+          value: 'ഞ',
+          devanagariLabel: 'ञ',
+          iso15919Label: 'ñ',
+        ).withGridPlacement(rowStart: 4, columnStart: 4),
+        const MalayalamKey(
+          value: 'ട',
+          devanagariLabel: 'ट',
+          iso15919Label: 'ṭ',
+        ).withGridPlacement(rowStart: 5, columnStart: 0),
+        const MalayalamKey(
+          value: 'ഠ',
+          devanagariLabel: 'ठ',
+          iso15919Label: 'ṭh',
+        ).withGridPlacement(rowStart: 5, columnStart: 1),
+        const MalayalamKey(
+          value: 'ഡ',
+          devanagariLabel: 'ड',
+          iso15919Label: 'ḍ',
+        ).withGridPlacement(rowStart: 5, columnStart: 2),
+        const MalayalamKey(
+          value: 'ഢ',
+          devanagariLabel: 'ढ',
+          iso15919Label: 'ḍh',
+        ).withGridPlacement(rowStart: 5, columnStart: 3),
+        const MalayalamKey(
+          value: 'ണ',
+          devanagariLabel: 'ण',
+          iso15919Label: 'ṇ',
+        ).withGridPlacement(rowStart: 5, columnStart: 4),
+        const MalayalamKey(
+          value: 'ത',
+          devanagariLabel: 'त',
+          iso15919Label: 't',
+        ).withGridPlacement(rowStart: 6, columnStart: 0),
+        const MalayalamKey(
+          value: 'ഥ',
+          devanagariLabel: 'थ',
+          iso15919Label: 'th',
+        ).withGridPlacement(rowStart: 6, columnStart: 1),
+        const MalayalamKey(
+          value: 'ദ',
+          devanagariLabel: 'द',
+          iso15919Label: 'd',
+        ).withGridPlacement(rowStart: 6, columnStart: 2),
+        const MalayalamKey(
+          value: 'ധ',
+          devanagariLabel: 'ध',
+          iso15919Label: 'dh',
+        ).withGridPlacement(rowStart: 6, columnStart: 3),
+        const MalayalamKey(
+          value: 'ന',
+          devanagariLabel: 'न',
+          iso15919Label: 'n',
+        ).withGridPlacement(rowStart: 6, columnStart: 4),
+        const MalayalamKey(
+          value: 'പ',
+          devanagariLabel: 'प',
+          iso15919Label: 'p',
+        ).withGridPlacement(rowStart: 7, columnStart: 0),
+        const MalayalamKey(
+          value: 'ഫ',
+          devanagariLabel: 'फ',
+          iso15919Label: 'ph',
+        ).withGridPlacement(rowStart: 7, columnStart: 1),
+        const MalayalamKey(
+          value: 'ബ',
+          devanagariLabel: 'ब',
+          iso15919Label: 'b',
+        ).withGridPlacement(rowStart: 7, columnStart: 2),
+        const MalayalamKey(
+          value: 'ഭ',
+          devanagariLabel: 'भ',
+          iso15919Label: 'bh',
+        ).withGridPlacement(rowStart: 7, columnStart: 3),
+        const MalayalamKey(
+          value: 'മ',
+          devanagariLabel: 'म',
+          iso15919Label: 'm',
+        ).withGridPlacement(rowStart: 7, columnStart: 4),
 
         // ------------------------Other-Consonants------------------------
 
