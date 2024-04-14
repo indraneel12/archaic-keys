b1f22b822be0ec4fc44c4bab6e077693468e54e9
Indraneel Rajeevan <e.indraneel@gmail.com>
2024-04-14 18:07:35 +0530

    added responsiveness to custom keyboard

--- app/lib/src/widgets/keyboards/custom_keyboard.dart
+++ app/lib/src/widgets/keyboards/custom_keyboard.dart
@@ -6,6 +6,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:app/src/constants/constants.dart';
+import 'package:app/src/utilities/double_scrollview.dart';
 
 import 'utilities/key_button.dart';
 import 'utilities/keyboard_control_bar.dart';
@@ -14,61 +15,67 @@ import 'utilities/rgb_border_animator.dart';
 class CustomKeyboard extends StatefulWidget {
   const CustomKeyboard({super.key});
 
-  double get minWidth => AppDimensions.minWidth * 0.90;
-  double get minHeight => AppDimensions.minHeight * 0.50;
-  double get maxWidth => AppDimensions.maxWidth * 1.00;
-  double get maxHeight => AppDimensions.maxHeight * 1.00;
+  static const minWidth = AppDimensions.minWidth * 0.90;
+  static const minHeight = AppDimensions.minHeight * 0.50;
+  static const maxWidth = AppDimensions.maxWidth * 1.00;
+  static const maxHeight = AppDimensions.maxHeight * 1.00;
 
   Widget renderBody(BuildContext context) {
-    return Placeholder(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  '<unimplemented-keyboard>',
+    return DoubleScrollview(
+      minWidth: CustomKeyboard.minWidth,
+      minHeight: CustomKeyboard.minHeight,
+      maxWidth: CustomKeyboard.maxWidth,
+      maxHeight: CustomKeyboard.maxHeight,
+      body: Placeholder(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    '<unimplemented-keyboard>',
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Global Width = ${MediaQuery.sizeOf(context).width}',
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Global Width = ${MediaQuery.sizeOf(context).width}',
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Global Height = ${MediaQuery.sizeOf(context).height}',
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Global Height = ${MediaQuery.sizeOf(context).height}',
+                  ),
                 ),
-              ),
-              RgbBorderAnimator(
-                child: KeyButton(
-                  label: 'Ctrl',
-                  isToggle: true,
-                  activeColor: Colors.blue,
+                RgbBorderAnimator(
+                  child: KeyButton(
+                    label: 'Ctrl',
+                    isToggle: true,
+                    activeColor: Colors.blue,
+                    onPressed: () => {},
+                  ),
+                ),
+                RgbBorderAnimator(
+                  child: KeyButton(
+                    label: 'അ',
+                    fontFamily: CustomFonts.notoSansMalayalam,
+                    onPressed: () => {},
+                  ),
+                ),
+                KeyButton(
+                  label: '𑐀',
+                  fontFamily: CustomFonts.notoSansNewa,
                   onPressed: () => {},
                 ),
-              ),
-              RgbBorderAnimator(
-                child: KeyButton(
-                  label: 'അ',
-                  fontFamily: CustomFonts.notoSansMalayalam,
-                  onPressed: () => {},
-                ),
-              ),
-              KeyButton(
-                label: '𑐀',
-                fontFamily: CustomFonts.notoSansNewa,
-                onPressed: () => {},
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
