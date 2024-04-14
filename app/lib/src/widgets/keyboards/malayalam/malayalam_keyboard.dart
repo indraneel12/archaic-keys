b35c13034181daa39aa4cbb43bb5c6598e3a92ae
Indraneel Rajeevan <e.indraneel@gmail.com>
2024-04-14 22:55:05 +0530

    added Layout system for Custom Keyboards

--- app/lib/src/widgets/keyboards/malayalam/malayalam_keyboard.dart
+++ app/lib/src/widgets/keyboards/malayalam/malayalam_keyboard.dart
@@ -5,6 +5,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+
 export 'malayalam_key.dart';
 
 class MalayalamKeyboard extends StatelessWidget {
@@ -13,6 +15,21 @@ class MalayalamKeyboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement Malayalam Keyboard layout
-    return const Placeholder();
+    return LayoutGrid(
+      areas: '''
+    red green blue
+    red green blue
+     .   .  blue
+  ''',
+      // Note that the number of columns and rows matches the grid above (3x3)
+      rowSizes: [1.fr, 1.fr, 1.fr],
+      columnSizes: [1.fr, 1.fr, 1.fr],
+      children: [
+        // Alternatively, an extension method on Widget is available
+        Container(color: Colors.red).inGridArea('red'),
+        Container(color: Colors.blue).inGridArea('blue'),
+        Container(color: Colors.green).inGridArea('green'),
+      ],
+    );
   }
 }
