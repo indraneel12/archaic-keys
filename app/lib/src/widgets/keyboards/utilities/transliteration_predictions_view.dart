789f1a0b647bd18e727c04cae43d02b1e4294472
Indraneel Rajeevan <e.indraneel@gmail.com>
2024-04-23 08:59:02 +0530

    implemented toggle support for Transliteration feature

--- app/lib/src/widgets/keyboards/utilities/transliteration_predictions_view.dart
+++ app/lib/src/widgets/keyboards/utilities/transliteration_predictions_view.dart
@@ -14,6 +14,11 @@ class TransliterationPredictionsView extends StatelessWidget {
   static const minWidth = 32.0;
   static const minHeight = 32.0;
 
+  static const defaultMessage =
+      'Transliteration suggestions powered by AI4Bharat';
+  static const errorMessage =
+      'Could not fetch the suggestions from AI4Bharat...';
+
   const TransliterationPredictionsView({
     super.key,
     this.alignment = Alignment.centerLeft,
@@ -39,59 +44,61 @@ class TransliterationPredictionsView extends StatelessWidget {
           alignment: alignment,
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Consumer<TextModel>(
+            child: Consumer<TransliterationModel>(
               builder: (context, model, child) {
-                return FutureBuilder<(List<String>, String, int, int)>(
-                    future: model.fetchTransliterationPredictions(
-                      Provider.of<KeyboardModel>(context, listen: false)
-                          .currentKeyboardId,
-                    ),
-                    builder: (
-                      BuildContext context,
-                      AsyncSnapshot<(List<String>, String, int, int)> snapshot,
-                    ) {
-                      if (!snapshot.hasData) {
-                        return const Text(
-                          'Transliteration suggestions powered by AI4Bharat',
-                        );
-                      }
-                      if (snapshot.hasError) {
-                        return const Text(
-                          'Could not fetch the predictions from AI4Bharat...',
-                        );
-                      }
-                      final (p, w, s, e) = snapshot.data!;
-                      return Row(
-                        children: <Widget>[
-                          for (final prediction in p)
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: ClipRect(
-                                child: SizedBox(
-                                  height:
-                                      TransliterationPredictionsView.minHeight,
-                                  child: InkWell(
-                                    borderRadius: const BorderRadius.all(
-                                      Radius.circular(16.0),
+                if (!model.isTransliterationOn) {
+                  return const Text(defaultMessage);
+                }
+                return Consumer<TextModel>(builder: (context, model, child) {
+                  return FutureBuilder<(List<String>, String, int, int)>(
+                      future: model.fetchTransliterationPredictions(
+                        Provider.of<KeyboardModel>(context, listen: false)
+                            .currentKeyboardId,
+                      ),
+                      builder: (
+                        BuildContext context,
+                        AsyncSnapshot<(List<String>, String, int, int)>
+                            snapshot,
+                      ) {
+                        if (!snapshot.hasData) {
+                          return const Text(defaultMessage);
+                        }
+                        if (snapshot.hasError) {
+                          return const Text(errorMessage);
+                        }
+                        final (p, w, s, e) = snapshot.data!;
+                        return Row(
+                          children: <Widget>[
+                            for (final prediction in p)
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: ClipRect(
+                                  child: SizedBox(
+                                    height: TransliterationPredictionsView
+                                        .minHeight,
+                                    child: InkWell(
+                                      borderRadius: const BorderRadius.all(
+                                        Radius.circular(16.0),
+                                      ),
+                                      child: FittedBox(
+                                        fit: BoxFit.fill,
+                                        child: Text(prediction),
+                                      ),
+                                      onTap: () {
+                                        model.chooseTransliterationPrediction(
+                                          prediction,
+                                          (w, s, e),
+                                        );
+                                      },
                                     ),
-                                    child: FittedBox(
-                                      fit: BoxFit.fill,
-                                      child: Text(prediction),
-                                    ),
-                                    onTap: () {
-                                      model.chooseTransliterationPrediction(
-                                        prediction,
-                                        (w, s, e),
-                                      );
-                                    },
                                   ),
                                 ),
                               ),
-                            ),
-                        ],
-                      );
-                    });
+                          ],
+                        );
+                      });
+                });
               },
             ),
           ),
